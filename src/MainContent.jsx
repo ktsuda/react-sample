c340a0997906d3526f8e9cec0e295e92c8cb344a
ktsuda <ktsuda@users.noreply.github.com>
2021-12-05 15:49:06 +0900

    Add sample components.

---
+++ src/MainContent.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+export class MainContent extends Component {
+  render() {
+    return (
+      <React.StrictMode>
+        <div class="m-1 p-1">Hello World!</div>
+      </React.StrictMode>
+    );
+  };
+};
+
+export default MainContent;
