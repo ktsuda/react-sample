ac6267990a129e5aaefe33ed6a8d28e8e3c21c60
ktsuda <ktsuda@users.noreply.github.com>
2021-12-07 16:09:33 +0900

    Update the MainContent component.
    Add the onRefreshClick callback.

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,13 +1,32 @@
 import React, {Component} from "react";
 
 export class MainContent extends Component {
+  state = {
+    pageTitle: "Customers",
+    customersCount: 5,
+  };
+
   render() {
     return (
       <React.StrictMode>
-        <div class="m-1 p-1">Hello World!</div>
+        <h4 className="border-bottom m-1 p-1">
+        {this.state.pageTitle}
+        <span className="badge bg-secondary m-2">
+          {this.state.customersCount}
+        </span>
+      <button className="btn btn-info" onClick={this.onRefreshClick}>
+        Refresh
+      </button>
+      </h4>
       </React.StrictMode>
     );
   };
+
+  onRefreshClick = () => {
+    this.setState({
+      customersCount: 7,
+    })
+  }
 };
 
 export default MainContent;
