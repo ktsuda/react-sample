6f6dc111be31810a89d2350b8f9b633f4d329fc9
ktsuda <ktsuda@users.noreply.github.com>
2021-12-07 16:14:59 +0900

    Add a table with sample data.

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -4,6 +4,13 @@ export class MainContent extends Component {
   state = {
     pageTitle: "Customers",
     customersCount: 5,
+    customers: [
+      {id:1,name:"Scott",phone:"123-456",address:{city:"New Delhi"}},
+      {id:2,name:"Jones",phone:"982-014",address:{city:"New York"}},
+      {id:3,name:"Allen",phone:"889-921",address:{city:"New Jersy"}},
+      {id:4,name:"James",phone:"",address:{city:"London"}},
+      {id:5,name:"John", phone:"",address:{city:"Tokyo"}},
+    ],
   };
 
   render() {
@@ -18,6 +25,31 @@ export class MainContent extends Component {
         Refresh
       </button>
       </h4>
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Customer Name</th>
+            <th>Phone</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.customers.map((cust) => {
+            return(
+              <tr key={cust.id}>
+                <td>{cust.id}</td>
+                <td>{cust.name}</td>
+                <td>
+                  {cust.phone?cust.phone:(<div className="bg-warning p-2 text-center">No Phone</div>)}
+                </td>
+                <td>{cust.address.city}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
       </React.StrictMode>
     );
   };
