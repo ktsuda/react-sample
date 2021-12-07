dc898fbfb9cd8274945783c989fdc2cada4bbfe1
ktsuda <ktsuda@users.noreply.github.com>
2021-12-08 03:32:46 +0900

    Implement styles with a separate method.

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -13,6 +13,15 @@ export class MainContent extends Component {
     ],
   };
 
+  customerNameStyle = (custName) => {
+    if (custName.startsWith('S')) return {backgroundColor: 'green'}
+    else if (custName.startsWith('J')) return {
+      backgroundColor: 'blue',
+      color: 'white',
+    }
+    else return {}
+  }
+
   render() {
     return (
       <React.StrictMode>
@@ -64,11 +73,7 @@ export class MainContent extends Component {
         <tr key={cust.id}>
         <td>{cust.id}</td>
         <td><img src={cust.photo} alt={cust.name} /></td>
-        <td style={{
-          backgroundColor:(cust.name.startsWith('S'))?'green':'red'
-        }}>
-          {cust.name}
-        </td>
+        <td style={this.customerNameStyle(cust.name)}>{cust.name}</td>
         <td>{this.getPhoneToRender(cust.phone)}</td>
         <td>{cust.address.city}</td>
         </tr>
