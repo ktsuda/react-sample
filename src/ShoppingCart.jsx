675e92d5f18d306aad1a54dbc2e744f9e28b1bfc
ktsuda <ktsuda@users.noreply.github.com>
2021-12-10 20:01:39 +0900

    Use the constructor.

--- src/ShoppingCart.jsx
+++ src/ShoppingCart.jsx
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 import Product from "./Product";
 
 export default class ShoppingCart extends Component {
-  state = {
-    products: [
-      { id: 1, productName: "iPhone", price: 8900, quantity: 0 },
-      { id: 2, productName: "Sony Camera", price: 4500, quantity: 0 },
-      { id: 3, productName: "Samsung QLED TV", price: 7745, quantity: 0 },
-      { id: 4, productName: "iPad Pro", price: 12400, quantity: 0 },
-      { id: 5, productName: "Xbox", price: 7780, quantity: 0 },
-      { id: 6, productName: "Dell Monitor", price: 880, quantity: 0 },
-    ],
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      products: [
+        { id: 1, productName: "iPhone", price: 8900, quantity: 0 },
+        { id: 2, productName: "Sony Camera", price: 4500, quantity: 0 },
+        { id: 3, productName: "Samsung QLED TV", price: 7745, quantity: 0 },
+        { id: 4, productName: "iPad Pro", price: 12400, quantity: 0 },
+        { id: 5, productName: "Xbox", price: 7780, quantity: 0 },
+        { id: 6, productName: "Dell Monitor", price: 880, quantity: 0 },
+      ],
+    }
   }
 
   render() {
@@ -56,7 +60,7 @@ export default class ShoppingCart extends Component {
   handleDelete = (prod) => {
     let allProducts = [...this.state.products] // copy the array
     let index = allProducts.indexOf(prod)
-    allProducts.splice(index,1)
+    allProducts.splice(index, 1)
     this.setState({ products: allProducts })
   }
 }
