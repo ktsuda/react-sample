8e813821d9f2e5856e69b7d73094f5b252a51cca
ktsuda <ktsuda@users.noreply.github.com>
2021-12-12 20:41:00 +0900

    Add dashboard component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
 // import ShoppingCart from "./ShoppingCart";
-import Login from "./Login"
+// import Login from "./Login"
+import Dashboard from "./Dashboard"
 
 export default class App extends Component {
   render() {
     return (
       <React.StrictMode>
         <NavBar />
-        {// <ShoppingCart />
+        {
+          // <ShoppingCart />
+          //<Login />
         }
-        <Login />
+      <Dashboard />
       </React.StrictMode>
     );
   };
